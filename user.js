c3100ca628461901ffd50e773ac34350e9b5ca70
atomGit <atomGit@users.noreply.github.com>
2017-12-21 12:51:22 +0000

    Update user.js

--- user.js
+++ user.js
@@ -7,7 +7,7 @@
  * credit   : ghacksuserjs/ghacks-user.js (https://github.com/ghacksuserjs/ghacks-user.js/blob/master/user.js)
  * web      : Firefox/Gecko Configuration Guide for Privacy Freaks and Performance Buffs (http://12bytes.org/articles/tech/firefox-gecko-config-for-privacy-freaks-and-and-performance-buffs)
  * code     : https://github.com/atomGit/ghacks-user.js
- * version  : 54r4 (based on Firefox v54)
+ * version  : 56r1 (based on Firefox v56)
  * author   : 12bytes.org
  *
  * IMPORTANT: this user.js is a suppliment that is intended to be appended to the ghacksuserjs/ghacks-user.js user.js file (https://github.com/ghacksuserjs/ghacks-user.js/blob/master/user.js)
@@ -16,13 +16,13 @@
  *
 */
 
-
-
 /*
  * === 12BYTES.ORG GHACKS DIFFS *** TEMPORARY *** ===
  *
  * these settings are temporary changes to the ghacks prefs which, in my case at least, cause problems
 */
+
+user_pref("_user.js.parrot", "syntax error @ 12BYTES.ORG GHACKS DIFFS *** TEMPORARY ***");
 
 user_pref("privacy.firstparty.isolate", false);           // 'true' breaks functionality of apparently several add-ons that deal with deleting cookies, local and indexDB storage
 user_pref("ui.use_standins_for_native_colors", false);    // 'true' cause tool-tip text to be the same color as the background color, at least on Linux
@@ -36,24 +36,19 @@ user_pref("ui.use_standins_for_native_colors", false);    // 'true' cause tool-t
 user_pref("_user.js.parrot", "syntax error @ 12BYTES.ORG GHACKS DIFFS");
 
 user_pref("browser.bookmarks.max_backups", 5);                  // i don't mind keeping a few extra backups of my bookmarks
-user_pref("browser.display.use_document_fonts", 1);             // '0' breaks the display of uBlock and uMatrix
+user_pref("browser.display.use_document_fonts", 1);             // '0' breaks the display of uBlock and uMatrix on Linux
 user_pref("browser.download.forbid_open_with", false);          // allow the `open with` option when downloading a file
 user_pref("browser.formfill.enable", true);                     // remember form and search history
 user_pref("browser.sessionstore.interval", 15000000);           // essentially disable session store (restore session after browser crash) to reduce disk writes
 user_pref("browser.sessionstore.max_tabs_undo", 3);             // allow to restore closed tabs
 user_pref("browser.urlbar.autoFill", true);                     // allow auto-complete of text entered in the address bar
 user_pref("browser.urlbar.autoFill.typed", true);               // allow auto-complete of text entered in the address bar
-user_pref("browser.urlbar.autocomplete.enabled", true);         // allow auto-complete of text entered in the address bar
 user_pref("browser.urlbar.suggest.bookmark", true);             // allow auto-complete of text entered in the address bar
 user_pref("browser.urlbar.suggest.history", true);              // allow auto-complete of text entered in the address bar
 user_pref("dom.event.clipboardevents.enabled", true);           // `false' breaks TinyMCE editor paste (Ctrl+V) such as is used by WordPress, possibly others
-user_pref("dom.indexedDB.enabled", true);                       // as ghacks said, disabling this will break some sites and extensions
 user_pref("dom.popup_allowed_events", "change click dblclick mouseup notificationclick reset submit touchend");  // allow all default JS pop-up events
 user_pref("dom.workers.enabled", true);                         // unbreak coin-hive.com login, some other sites
-user_pref("extensions.systemAddon.update.url", "");             // disallow updates for system add-ons (Firefox Features) that ship with the browser
 user_pref("full-screen-api.enabled", true);                     // enable full-screen video capability
-user_pref("full-screen-api.warning.timeout", 0);                // disable full-screen warning
-user_pref("full-screen-api.warning.delay", 0);                  // how long to display a warning ("YouTube is now Fullscreen") when a site enters full-screen mode - `0` disables the warning
 user_pref("layout.css.visited_links_enabled", true);            // though it is a privacy trade-off, differentiating between visited and not visited links is important to me
 user_pref("layout.spellcheckDefault", 2);                       // enable spell-check for single line inputs
 //user_pref("javascript.options.wasm", true);                   // unbreak coin-hive.com login
@@ -63,14 +58,11 @@ user_pref("privacy.clearOnShutdown.history", false);            // keep history 
 user_pref("privacy.clearOnShutdown.formdata", false);           // keep form data on shutdown
 user_pref("privacy.cpd.formdata", false);                       // don't select form data when clearing history manually
 user_pref("privacy.cpd.history", false);                        // don't select history item when clearing history manually
-user_pref("privacy.resistFingerprinting.block_mozAddonManager", true);  // allow WebExtensions to work on websites operated by Mozilla (FF 57 +)
-user_pref("reader.parse-on-load.enabled", false);               // disable Reader
-user_pref("security.tls.version.min", 1);                       // in my case a value of "2" breaks cPanel login
-user_pref("security.OCSP.enabled", 0);                          // disable on-line security check for SSL certificates (can cause sites to load slower or not at all if the service is down - you should typically NOT disable this)
-user_pref("svg.disabled", false);                               // `true` breaks YouTube player
-user_pref("webgl.disabled", false);                             // `true` will break some sites including Google Earth view on Google Maps - WenGL appears to be dependant upon JS being enabled (which should be disabled globally by default) so if you trust a site enough to enable JS, you may as well enable WebGL as well since the security risks with WebGL appear to be far less of a problem than JS
-user_pref("webgl.disable-extensions", false);                   // 'true' breaks Google Earth view on Google Maps
-user_pref("webgl.min_capability_mode", false);                  // 'true' breaks Google Earth view on Google Maps
+user_pref("security.tls.version.min", 1);                       // in my case a value of '3' breaks cPanel login
+user_pref("security.OCSP.enabled", 0);                          // disable on-line verification of SSL certificates (can cause sites to load slower or not at all if the service is down - you should typically NOT disable this)
+user_pref("webgl.disabled", false);                             // `true` will break some sites including Google Earth Street View on Google Maps - WenGL appears to be dependant upon JS being enabled (which should be disabled globally by default) so if you trust a site enough to enable JS, you may as well enable WebGL as well since the security risks with WebGL appear to be far less of a problem than JS
+user_pref("webgl.disable-extensions", false);                   // 'true' breaks Google Earth Street View on Google Maps
+user_pref("webgl.min_capability_mode", false);                  // 'true' breaks Google Earth Street View on Google Maps
 
 /*
  * === 12BYTES.ORG LINUX SPECIFIC ===
@@ -86,6 +78,33 @@ user_pref("network.protocol-handler.warn-external.apt", true);
 user_pref("network.protocol-handler.warn-external.apt+http", true);
 
 /*
+ * === 12BYTES.ORG MISC ===
+*/
+
+user_pref("_user.js.parrot", "syntax error @ 12BYTES.ORG MISC");
+
+user_pref("accessibility.typeaheadfind", false);                        // whether to open the find bar to search for text as soon as you start typing
+user_pref("browser.feeds.showFirstRunUI", false);                       // don't show feed intro when first subscribing to a news feed
+user_pref("browser.startup.homepage", "http://12bytes.org/");           // page to display when clicking the Home button
+user_pref("browser.startup.page", 3);                                   // page to display on startup - 1=home, 2=blank, 3=restore last session
+user_pref("browser.triple_click_selects_paragraph", false);             // whether to select paragraphs when triple clicked
+user_pref("browser.urlbar.formatting.enabled", false);                  // disable domain highlighting in the location bar
+user_pref("devtools.toolbox.zoomValue", "1.3");                         // font size for the Developers Toolbox
+user_pref("dom.event.contextmenu.enabled", false);                      // avoids having to press Shift key to display the browser context menu which may be replaced by JS
+user_pref("extensions.systemAddon.update.url", "");                     // disallow updates for system add-ons (Firefox Features) that ship with the browser - should probably not make this change if you use system add-ons
+user_pref("full-screen-api.warning.timeout", 0);                        // disable full-screen warning
+user_pref("full-screen-api.warning.delay", 0);                          // how long to display a warning ("YouTube is now Fullscreen") when a site enters full-screen mode - `0` disables the warning
+user_pref("general.autoScroll", false);                                 // Whether to enable auto-scrolling (middle-click on a page to display scroll map)
+user_pref("image.animation_mode", "once");                              // disable repetitive image (GIF) animation
+user_pref("layout.css.devPixelsPerPx", "1.3");                          // global UI zoom level - this affects toolbars, icons, web pages, etc.
+user_pref("layout.word_select.eat_space_to_next_word", false);          // whether to include spaces after a word when double clicking to select the word
+user_pref("layout.word_select.stop_at_punctuation", true);              // whether to stop selection at a punctuation when double clicking to select a word
+user_pref("mousewheel.with_shift.action", 0);                           // don't do anything when the Shift key is used with the mouse wheel
+user_pref("nglayout.enable_drag_images", false);                        // whether images can be dragged - also seems to have an effect on highlighting and dragging text - this feature can be very annoying, especially for website editors/admins
+user_pref("privacy.resistFingerprinting.block_mozAddonManager", true);  // allow WebExtensions to work on websites operated by Mozilla (FF 57 +)
+user_pref("reader.parse-on-load.enabled", false);                       // disable Reader
+
+/*
  * === 12BYTES.ORG TABS ===
 */
 
@@ -95,11 +114,12 @@ user_pref("accessibility.tabfocus", 3);                         // 3: Tab key fo
 user_pref("browser.link.open_newwindow", 1);                    // controls when a new window/tab should be opened - 1=open links that open in a new window in the current tab, 2=open links that open in a new window in a new window, 3=open links that open in a new window in a new tab in the current window
 user_pref("browser.link.open_newwindow.override.external", 3);  // open links from external programs in: -1=default, 1=the current tab, 2=a new window, 3=a new tab
 user_pref("browser.sessionhistory.max_entries", 5);             // tab specific max number of pages that can be traversed when moving forward/backward in history - affects total memory consumption
+user_pref("browser.tabs.closeWindowWithLastTab", false);        // don't quit the browser when the last tab is closed
 user_pref("browser.tabs.loadDivertedInBackground", true);       // cause links opened from external programs to open in a new background tab
 user_pref("browser.tabs.loadInBackground", false);              // focus new tabs instead of loading them in the background
 
 /*
- * === 12BYTES.ORG SEARCH ENGINE SANITIZE ===
+ * === 12BYTES.ORG SEARCH ENGINES ===
 */
 
 user_pref("browser.search.param.yahoo-fr", "");     // sanitize Yahoo
@@ -112,27 +132,6 @@ user_pref("browser.search.param.yahoo-fr-ja", "");  // ^
 user_pref("font.minimum-size.x-western", 14);    // minimum font size
 user_pref("font.size.fixed.x-western", 16);      // monospace font size
 user_pref("font.size.variable.x-western", 18);   // default font size
-
-/*
- * === 12BYTES.ORG MISC ===
-*/
-
-user_pref("_user.js.parrot", "syntax error @ 12BYTES.ORG MISC");
-
-user_pref("accessibility.typeaheadfind", false);                 // whether to open the find bar to search for text as soon as you start typing
-user_pref("browser.feeds.showFirstRunUI", false);                // don't show feed intro when first subscribing to a news feed
-user_pref("browser.fullscreen.autohide", false);                 // whether to hide tool bars and tabs when entering full-screen view (F11)
-user_pref("browser.startup.homepage", "http://12bytes.org/");    // page to display when clicking the Home button
-user_pref("browser.startup.page", 3);                            // page to display on startup - 1=home, 2=blank, 3=restore last session
-user_pref("browser.triple_click_selects_paragraph", false);      // whether to select paragraphs when triple clicked
-user_pref("general.autoScroll", false);                          // Whether to enable auto-scrolling (middle-click on a page to display scroll map)
-user_pref("image.animation_mode", "once");                       // disable repetitive image (GIF) animation
-user_pref("layout.word_select.eat_space_to_next_word", false);   // whether to include spaces after a word when double clicking to select the word
-user_pref("layout.word_select.stop_at_punctuation", true);       // whether to stop selection at a punctuation when double clicking to select a word
-user_pref("mousewheel.with_shift.action", 0);                    // controls what happens when the Shift key is pressed and the mouse wheel is scrolled
-user_pref("nglayout.enable_drag_images", false);                 // whether images can be dragged - also seems to have an effect on highlighting and dragging text - this feature can be very annoying, especially for website editors/admins
-user_pref("devtools.toolbox.zoomValue", "1.3");                  // font size for the Developers Toolbox
-user_pref("layout.css.devPixelsPerPx", "1.3");                   // global UI zoom level - this affects toolbars, icons, web pages, etc.
 
 /*
  * === 12BYTES.ORG SMOOTH SCROLLING ===
